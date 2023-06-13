04211f3d2f034d0341830594c945ba9f9b4d2778
FilhoDeEd <edson.er.rodrigues@gmail.com>
2023-06-13 20:31:18 -0300

    primeiro commit

---
+++ src/inserts_generator.py
@@ -0,0 +1,114 @@
+import pandas as pd
+import json
+from typing import List, Callable
+
+
+def inserts_generator(table_name: str, dataFrameCsvPath: str, insertDefinitionPath: str, derivateFunctions: List[Callable] = None):
+    """
+@ Explicar como funciona a função de forma geral.
+
+• table_name:
+    Nome da tabela que irá receber os inserts. Esse nome será utilizado para nomear o JSON que contém as strings de insert values.
+
+• dataFrameCsvPath:
+    Caminho para o csv que contém os dados. Garanta que o Pandas pode ler esse arquivo por meio do método pandas.read_csv().
+
+• insertDefinitionPath:
+    Caminho para o JSON que especifica a definição da tabela
+
+• derivateFunctions:
+    As funções em derivateFunction devem receber um Pandas Series do mesmo tipo que o especificado em dataFrameCsvPath.
+    Além disso, a função deve retornar exatamente a string a ser inclusa em nas strings de INSERT. Mesmo as aspas simples
+    próprias de campos textuais devem ser inclusas nessa string de retorno. Você deve tratar possíveis NaN na sua função.
+    Nesses casos, retorne a string NULL. A não ser que seu atributo derivado seja uma Cluster Key, nesse caso, retorne o
+    que for adequado (talvez um valor padrão). 
+    """
+
+    dataFrame = pd.read_csv(dataFrameCsvPath)
+
+    with open(insertDefinitionPath) as file:
+        insertDefinition: dict[str, str] = json.load(file)
+
+    columns = insertDefinition.keys()
+
+    insertStrings = []
+
+    for index, row in dataFrame.iterrows():
+
+        insertString = '('
+
+        for column in columns:
+
+            type = insertDefinition[column]
+            
+            if 'der_' in type:
+
+                dFunc_name = type.split('_')[1]
+                dFunc = None
+
+                for func in derivateFunctions:
+                    if func.__name__ == dFunc_name:
+                        dFunc = func
+                        break
+                    
+                if dFunc == None:
+                    raise ValueError(f"A função '{dFunc_name}' não foi encontrada na lista de funções.")
+
+                value = dFunc(row)
+                insertString = insertString + value + ','
+
+            else:
+
+                isClusterKey = False
+
+                if 'clusK_' in type:
+
+                    isClusterKey = True
+                    type,defaultValue = type.split('_')[1:]
+
+                if pd.isna(row[column]):
+                    
+                    if isClusterKey:
+
+                        if type == 'str' or type == 'date':
+                            insertString = insertString + f"'{defaultValue}'" + ','
+                        else:
+                            insertString = insertString + defaultValue + ','
+
+                    else:
+                        insertString = insertString + 'NULL' + ','
+
+                elif type == 'set' or type == 'list':
+                    
+                    multval = row[column].split(',')
+                    multval = [value.strip() for value in multval]
+                    multval = [value.replace("'",'\u2019') for value in multval]
+
+                    if type == 'set':
+                        insertString = insertString + str(set(multval)) + ','
+                    else:
+                        insertString = insertString + str(multval) + ','
+
+                elif type == 'date':
+
+                    if '-' not in row[column]:
+                        insertString = insertString + f"'{row[column]}-1-1'" + ','
+                    else:
+                        insertString = insertString + f"'{row[column]}'" + ','
+
+                elif type == 'str':
+
+                    stringText = row[column].replace("'",'\u2019')
+                    insertString = insertString + f"'{stringText}'" + ','
+                
+                else:
+                    insertString = insertString + str(row[column]) + ','
+
+        insertString = insertString.rstrip(',')
+        insertString = insertString + ')'
+        insertStrings.append(insertString)
+    
+    json_insertStrings = json.dumps(insertStrings, separators=(",\n", ": "))
+
+    with open(f'insert_values/{table_name}.json', 'w') as file:
+        file.write(json_insertStrings)
---
+++ src/main.py
@@ -0,0 +1,94 @@
+import json
+import re
+from cassandra.cluster import Cluster
+from inserts_generator import inserts_generator
+from cassandra.query import BatchStatement
+from pandas import Series, isna
+
+
+def getMainGenre(row: Series) -> str:
+
+      if isna(row['genre']):
+            return 'undefined'
+      else:
+            multval = row['genre'].split(',')
+            multval = [value.strip() for value in multval]
+            mainGenre = multval[0]
+            mainGenre = f"'{mainGenre}'"
+            return mainGenre
+
+def getMainCountry(row: Series) -> str:
+
+      if isna(row['country']):
+            return 'undefined'
+      else:
+            multval = row['country'].split(',')
+            multval = [value.strip() for value in multval]
+            mainCountry = multval[0]
+            mainCountry = f"'{mainCountry}'"
+            return mainCountry
+
+def parseBudgetToFloat(row: Series) -> str:
+
+      if isna(row['budget']):
+            return 'NULL'
+      else:
+            value = row['budget']
+            value = re.sub(r'[^0-9]', '', value)
+            value = float(value)
+            return str(value)
+
+def parseIncomeToFloat(row: Series) -> str:
+      
+      if isna(row['worldwide_gross_income']):
+            return 'NULL'
+      else:
+            value = row['worldwide_gross_income']
+            value = re.sub(r'[^0-9]', '', value)
+            value = float(value)
+            return str(value)
+      
+
+database_name = 'trabalho_bd2'
+keyspace_name = 'tables'
+table_name = 'film_by_genre'
+batchSize = 500
+tuplesAmount = 25000
+derivateFunctions = [getMainGenre, parseBudgetToFloat, parseIncomeToFloat]
+
+def main():
+      inserts_generator(table_name, 'csv/all_df.csv', f'insert_definitions/{table_name}_def.json',derivateFunctions)
+
+      cluster = Cluster(['localhost'])
+      session = cluster.connect()
+
+      with open(f'insert_values/{table_name}.json') as file:
+            insertStringsValues = json.load(file)
+
+      with open(f'insert_definitions/{table_name}_def.json') as file:
+            insertDefinition = json.load(file)
+
+      columns = insertDefinition.keys()
+
+      insertStringSchema = f'INSERT INTO {keyspace_name}.{table_name} ('
+
+      for column in columns:
+            insertStringSchema = insertStringSchema + column + ','
+
+      insertStringSchema = insertStringSchema.rstrip(',')
+      insertStringSchema = insertStringSchema + ') VALUES '
+
+      index = 0
+      batch = BatchStatement()
+      for i in range(int(tuplesAmount/batchSize)):
+            batch.clear()
+            for j in range(batchSize):
+                  insertString = insertStringSchema + insertStringsValues[index] + ';'
+                  batch.add(insertString)
+                  index += 1
+
+            session.execute(batch)
+
+
+if __name__ == '__main__': 
+      main()
